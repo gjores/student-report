525e7c72f1c3b96a559e77bc293181f3dff53794
gjorPe <gjores@gmail.com>
2014-06-17 20:33:00 +0200

    Added registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Studentreport::Application.routes.draw do
   resources :students
 
   get "welcome/index"
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
